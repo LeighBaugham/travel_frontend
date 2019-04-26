f839a4096a2514927645f7314b8a627b9ee5d73e
Leigh Baugham <leigh.baugham@gmail.com>
2019-04-26 11:54:23 -0400

    firts round

---
+++ src/components/LoggedOutHeader.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Header, Segment, Image } from 'semantic-ui-react'
+import background from '../assets/background.png'
+
+
+class LoggedOutHeader extends Component {
+
+    render () {
+        return(
+            <div>
+            
+                <Segment clearing>
+                <Header floated='left'>
+                <Header.Content>
+                            <Image src={background}/>
+                            {/* <Header.Subheader> Welcome {this.props.user}</Header.Subheader> */}
+                </Header.Content>
+                </Header>
+                </Segment>                
+                        
+            </div>
+            
+        )
+    }
+
+
+
+}
+
+
+export default LoggedOutHeader
---
+++ src/components/SignUp.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import { Button, Form, Grid, Header, Segment } from 'semantic-ui-react'
+
+
+
+class SignUp extends React.Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      name: "",
+      image_url: "",
+      password: "",
+      email: "",
+      phone: "",
+      location: ""
+    }
+  }
+  
+
+  
+
+  saveUser = (event) => {
+    event.preventDefault()
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(this.state)
+    }).then(res => res.json())
+   .then(user =>{ 
+     this.props.history.push("/")
+     this.props.setUser(user)
+    })
+    event.target.reset()
+  }
+
+  render = () =>
+
+  <div className='login-form'>
+  <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
+    <Grid.Column style={{ maxWidth: 450 }}>
+      
+      <Header as='h2' color='blue' textAlign='center'>
+        Create New Account
+      </Header>
+      {/* <p><p> {this.props.errors ? this.props.errors.error : null} </p></p> */}
+      <Form size='large' onSubmit={this.saveUser}>
+        <Segment stacked>
+          <Form.Input fluid icon='user' iconPosition='left' className="name" placeholder='Username' onChange={(e) => this.setState({ name: e.target.value })}/>
+          <Form.Input fluid icon='picture' iconPosition='left' className="image" placeholder='Picture' onChange={(e) => this.setState({ image_url: e.target.value })}/>
+
+          <Form.Input
+            fluid
+            icon='lock'
+            iconPosition='left'
+            placeholder='Password'
+            type='password'
+            name="password"
+            onChange={(e) => this.setState({ password: e.target.value })} />
+          <Form.Input fluid icon='envelope square' iconPosition='left' className="email" placeholder='Email' onChange={(e) => this.setState({ email: e.target.value })}/>
+          <Form.Input fluid icon='phone' iconPosition='left' className="phone" placeholder='Phone Number' onChange={(e) => this.setState({ phone: e.target.value })}/>
+          <Form.Input fluid icon='location arrow' iconPosition='left' className="location" placeholder='Location' onChange={(e) => this.setState({ location: e.target.value })}/>
+
+          <Button color='blue' fluid size='large'>
+            Submit
+          </Button>
+        </Segment>
+      </Form>
+    </Grid.Column>
+  </Grid>
+</div>
+}
+
+const SignUpWithRouter = withRouter(SignUp)
+
+export default SignUpWithRouter;
