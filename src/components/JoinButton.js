b3db169eadd551c7e78f1a474aab677c1b65e116
Leigh Baugham <leigh.baugham@gmail.com>
2019-05-02 11:29:29 -0400

    users can delete trips

--- src/components/JoinButton.js
+++ src/components/JoinButton.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react'
+import { Button, Popup } from 'semantic-ui-react'
 
+const timeoutLength = 2500
 
 
 class JoinButton extends Component {
 
+
+    state = { isOpen: false }
+
+    handleOpen = () => {
+      this.setState({ isOpen: true })
+  
+      this.timeout = setTimeout(() => {
+        this.setState({ isOpen: false })
+      }, timeoutLength)
+    }
+  
+    handleClose = () => {
+      this.setState({ isOpen: false })
+      clearTimeout(this.timeout)
+    }
+
     render () {
         return(
             <div>
-            
-            <Button onClick={() => this.props.joinPassport(this.props.trip.id)} basic color='green'>
-                Join
-            </Button>         
+                <Popup
+                    trigger={<Button onClick={() => this.props.joinPassport(this.props.trip.id)} basic color='green'>
+                                Join
+                            </Button>  }
+                    content={"Buckle Up!"}
+                    on='click'
+                    open={this.state.isOpen}
+                    onClose={this.handleClose}
+                    onOpen={this.handleOpen}
+                    position='top right'
+                />
                         
             </div>
             
